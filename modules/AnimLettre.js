204adb20c8a17058be6496badb8849cd2e60e571
Kelang <Kelang@users.noreply.github.com>
2019-02-13 19:24:35 -0500

    Ajout de la classe AnimLettre

---
+++ modules/AnimLettre.js
@@ -0,0 +1,9 @@
+class AnimLettre{
+
+    constructor(lesLettres, elmHeader, finAnim){
+        this.lesLettres = lesLettres;
+        this.elmHeader = elmHeader;
+        this.finAnim = finAnim;
+
+    }
+}
